826221b04394fa3b2560e4ca22fbf406070bd1f5
StephenPrayogo <stephenprayogo@gmail.com>
2016-05-19 15:31:31 +1000

    Build the changes from previous commit
    Build the changes from previous commit

--- dist/FitImage.js
+++ dist/FitImage.js
@@ -1,4 +1,4 @@
-Object.defineProperty(exports,"__esModule",{value:true});var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value" in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor;};}();var _react=require('react');var _react2=_interopRequireDefault(_react);
+Object.defineProperty(exports,"__esModule",{value:true});var _jsxFileName='src/FitImage.js';var _createClass=function(){function defineProperties(target,props){for(var i=0;i<props.length;i++){var descriptor=props[i];descriptor.enumerable=descriptor.enumerable||false;descriptor.configurable=true;if("value" in descriptor)descriptor.writable=true;Object.defineProperty(target,descriptor.key,descriptor);}}return function(Constructor,protoProps,staticProps){if(protoProps)defineProperties(Constructor.prototype,protoProps);if(staticProps)defineProperties(Constructor,staticProps);return Constructor;};}();var _react=require('react');var _react2=_interopRequireDefault(_react);
 
 
 
@@ -101,7 +101,9 @@ style:[
 this.props.style,
 this.getStyle()],
 
-onLayout:function onLayout(event){return _this3.resize(event);}}));}}]);return FitImage;}(_react.Component);
+onLayout:function onLayout(event){return _this3.resize(event);},__source:{fileName:_jsxFileName,lineNumber:97}},
+
+this.props.children));}}]);return FitImage;}(_react.Component);
 
 
 
